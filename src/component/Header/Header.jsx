eb70adfba00a952d99d00c25920bd7b575ae42e9
Boris <seikenfr@yahoo.fr>
2021-09-03 14:02:09 +0200

    added:  props to children

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,11 @@
 import { SearchBar } from 'component/SearchBar/SearchBar'
-export const Header = ()=> {
+export const Header = ({input, onChangeInput})=> {
   return( <>
       <header>
         <h1 className="header-title">WeatherNow</h1>
         <p className='header-content'>La méteo? D'accord.. <br />Mais de quelle ville? </p>
       </header>
-      <SearchBar/>
+      <SearchBar input={input} onChangeInput={onChangeInput} />
     </>
   )
 }
