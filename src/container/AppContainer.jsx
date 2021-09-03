9941443f04f1e6622d70bd93c0d365e7f83b7927
Boris <seikenfr@yahoo.fr>
2021-09-03 13:50:09 +0200

    added usestate and useEffect

--- src/container/AppContainer.jsx
+++ src/container/AppContainer.jsx
@@ -2,11 +2,15 @@ import './AppContainer.scss';
 import { Navbar } from 'component/NavBar/NavBar';
 import { Header } from 'component/Header/Header';
 import { WeatherResult } from 'component/WeatherResult/WeatherResult';
+
 export const App =() =>{
+  const [input, setInput] = useState('paris')
+  const onChangeInput = (e) => setInput(e.target.value)
+  const [datas, setDatas] = useState("")
 
   return(
       <div id="container-app">
-        <Navbar/>
+       <Navbar/>
        <Header/>
        <WeatherResult/>
       </div>
