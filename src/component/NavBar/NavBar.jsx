874f10c4eb8938460893c6a39640a0238820ca76
Boris <seikenfr@yahoo.fr>
2021-09-03 16:39:04 +0200

    added: elements icon + added to navbar

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,14 +1,17 @@
-import './NavBar.scss'
+import './NavBar.scss';
+import icon from 'elements/Icon/cloudy.svg'
 export const Navbar = () => {
   return(<>
     <nav id='nav'>
       <ul>
         <li className='nav-brand'>
+          <img src={icon} alt="illustration d'un soleil caché par un nuage" />
         </li>
         <li>
           <p>WN</p>
           </li>
         <li className='nav-brand'>
+        <img src={icon} alt="illustration d'un soleil caché par un nuage" />
         </li>
       </ul>
     </nav>
