370c45e2414b78457c2c99ecf95cc6b88fff1033
Boris <seikenfr@yahoo.fr>
2021-09-03 13:24:28 +0200

    added: SearchBar into component

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,11 @@
-
+import { SearchBar } from 'component/SearchBar/SearchBar'
 export const Header = ()=> {
   return( <>
       <header>
         <h1 className="header-title">WeatherNow</h1>
         <p className='header-content'>La méteo? D'accord.. <br />Mais de quelle ville? </p>
       </header>
+      <SearchBar/>
     </>
   )
 }
