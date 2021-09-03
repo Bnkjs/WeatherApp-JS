d63945fe1eab82d0cdfd281336e6fe393ca6a8c5
Boris <seikenfr@yahoo.fr>
2021-09-03 11:39:05 +0200

    added: SearchBar component

---
+++ src/component/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+function SearchBar(){
+  const [input, setInput] = useState('paris')
+  const onChangeInput = (e) => setInput(e.target.value)
+
+  return(<div id='searchBar'>
+     <form id="form-weather">
+       <input 
+        type="text" 
+        name='search-input' 
+        placeholder='Paris, Antalya, Lomé...' 
+        className='search-input' 
+        onChange={onChangeInput}
+        value={input}
+       />
+     </form>
+     <button>Rechercher</button>
+  </div>)
+}
+
+export default SearchBar
